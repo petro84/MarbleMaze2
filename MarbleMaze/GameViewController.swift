2565354f4946c34b3dd25d193e564988efaefda7
Larry Petroski <larry.petroski@yahoo.com>
2018-07-19 18:44:22 -0500

    Chapter 14 completed

--- MarbleMaze/GameViewController.swift
+++ MarbleMaze/GameViewController.swift
@@ -13,6 +13,13 @@ class GameViewController: UIViewController {
 
     var scnView: SCNView!
     var scnScene: SCNScene!
+    var ballNode: SCNNode!
+    
+    let CollisionCategoryBall = 1
+    let CollisionCategoryStone = 2
+    let CollisionCategoryPillar = 4
+    let CollisionCategoryCrate = 8
+    let CollisionCategoryPearl = 16
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,10 +43,13 @@ class GameViewController: UIViewController {
         scnView.showsStatistics = true
         
         scnScene = SCNScene(named: "art.scnassets/game.scn")
+        scnScene.physicsWorld.contactDelegate = self
         scnView.scene = scnScene
     }
     
     func setupNodes() {
+        ballNode = scnScene.rootNode.childNode(withName: "ball", recursively: true)!
+        ballNode.physicsBody?.contactTestBitMask = CollisionCategoryPillar | CollisionCategoryCrate | CollisionCategoryPearl
     }
     
     func setupSounds() {
@@ -48,5 +58,27 @@ class GameViewController: UIViewController {
 
 extension GameViewController: SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    }
+}
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        var contactNode: SCNNode!
+        
+        if contact.nodeA.name == "ball" {
+            contactNode = contact.nodeB
+        } else {
+            contactNode = contact.nodeA
+        }
+        
+        if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPearl {
+            contactNode.isHidden = true
+            contactNode.runAction(SCNAction.waitForDurationThenRunBlock(duration: 30) {
+                (node:SCNNode!) -> Void in node.isHidden = false
+            })
+        }
+        
+        if contactNode.physicsBody?.categoryBitMask == CollisionCategoryPillar || contactNode.physicsBody?.categoryBitMask == CollisionCategoryCrate {
+        }
     }
 }
